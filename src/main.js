22e3a0e93e231c94d36b029a592f16782f3fd505
cmy <1732728869@qq.com>
2019-12-23 14:23:43 +0800

    配置路由文件初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//导入路由模块
+import router from './router/index'
+
+//导入element组件
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
+
+
+
+//这是日志的详细
 Vue.config.productionTip = false
 
+
 new Vue({
+  router,//挂载路由
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,9 @@
+//导入路由模块
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+Vue.use(VueRouter)
+//路由规则配置
+const router = new VueRouter({
+  routes:[]
+})
+export default router
